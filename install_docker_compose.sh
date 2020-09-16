ec4008fe51308dacefa5a59268167a50ec2c2c67
Stanley <stanleytiu7@gmail.com>
2020-09-15 23:57:11 -0400

    update install docker script

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -1,1 +1,4 @@
-curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh && udo usermod -aG docker s2 && pip3 install docker-compose
+curl -fsSL https://get.docker.com -o get-docker.sh 
+&& sudo sh get-docker.sh 
+&& sudo usermod -aG docker s2 
+&& pip3 install docker-compose
